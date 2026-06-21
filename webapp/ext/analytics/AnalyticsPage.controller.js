c8daeebb9d022f2e6ab52cf272c7f62bada15d7f
Jose Casas Mejia <josecasasmejia97@gmail.com>
2026-06-22 01:29:19 +0200

    refactor: replace macros:Chart with VizFrame for stable chart type switching

--- webapp/ext/analytics/AnalyticsPage.controller.js
+++ webapp/ext/analytics/AnalyticsPage.controller.js
@@ -1,51 +1,77 @@
 sap.ui.define([
     "sap/fe/core/PageController",
-    "sap/ui/model/json/JSONModel"
-], function (PageController, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/viz/ui5/controls/common/feeds/FeedItem"
+], function (PageController, JSONModel, FeedItem) {
     "use strict";
 
     return PageController.extend("maintnoti.maint.ext.analytics.AnalyticsPage", {
+
         onInit: function () {
             PageController.prototype.onInit.apply(this, arguments);
-            this._kpiLoaded = false;
-            this.getView().setModel(new JSONModel({ count: "...", totalHours: "...", openCount: "..." }), "kpi");
-        },
+            var oView = this.getView();
+            oView.setModel(new JSONModel({ count: 0, totalHours: 0, openCount: 0 }), "kpi");
+            oView.setModel(new JSONModel({ data: [] }), "chartStatus");
+            oView.setModel(new JSONModel({ data: [] }), "chartPriority");
 
-        onAfterRendering: function () {
-            if (PageController.prototype.onAfterRendering) {
-                PageController.prototype.onAfterRendering.apply(this, arguments);
-            }
-            if (this._kpiLoaded) { return; }
             var oModel = this.getOwnerComponent().getModel();
             if (oModel) {
-                this._loadKpis(oModel);
+                oModel.getMetaModel().requestObject("/MaintNotificationAnal/").then(function () {
+                    this._loadAllData(oModel);
+                }.bind(this)).catch(function () {});
             }
         },
 
-        _loadKpis: function (oModel) {
-            this._kpiLoaded = true;
+        _loadAllData: function (oModel) {
             var oView = this.getView();
-            var oListBinding = oModel.bindList("/MaintNotificationAnal");
-            oListBinding.requestContexts(0, 500).then(function (aCtx) {
+            var oBinding = oModel.bindList("/MaintNotificationAnal");
+
+            oBinding.requestContexts(0, 500).then(function (aCtx) {
                 if (oView.bIsDestroyed) { return; }
-                var total = 0;
-                var openCount = 0;
+
+                var total = 0, openCount = 0;
+                var statusMap = {}, priorityMap = {};
+
                 aCtx.forEach(function (ctx) {
-                    total += parseFloat(ctx.getProperty("SlaHours") || 0);
-                    if (ctx.getProperty("Status") === "101") {
-                        openCount++;
-                    }
+                    var hours    = parseFloat(ctx.getProperty("SlaHours") || 0);
+                    var status   = ctx.getProperty("Status")       || "?";
+                    var stText   = ctx.getProperty("StatusText")   || status;
+                    var priority = ctx.getProperty("Priority")     || "?";
+                    var prText   = ctx.getProperty("PriorityText") || priority;
+
+                    total += hours;
+                    if (status === "101") { openCount++; }
+
+                    if (!statusMap[status])   { statusMap[status]   = { StatusText:   stText, SlaHours: 0 }; }
+                    if (!priorityMap[priority]){ priorityMap[priority]= { PriorityText: prText, SlaHours: 0 }; }
+                    statusMap[status].SlaHours   += hours;
+                    priorityMap[priority].SlaHours += hours;
                 });
-                oView.getModel("kpi").setData({
-                    count: aCtx.length,
-                    totalHours: total,
-                    openCount: openCount
-                });
-            }).catch(function () {
-                // KPIs remain at default "..."
-            }).finally(function () {
-                oListBinding.destroy();
-            });
+
+                oView.getModel("kpi").setData({ count: aCtx.length, totalHours: total, openCount: openCount });
+                oView.getModel("chartStatus").setProperty("/data",   Object.values(statusMap));
+                oView.getModel("chartPriority").setProperty("/data", Object.values(priorityMap));
+
+            }).catch(function () {}).finally(function () { oBinding.destroy(); });
+        },
+
+        onChartTypeChange: function (oEvent) {
+            var sKey      = oEvent.getParameter("item").getKey();
+            var sId       = oEvent.getSource().getId();
+            var bStatus   = sId.indexOf("typeStatusBtn") !== -1;
+            var oViz      = this.byId(bStatus ? "vizStatus" : "vizPriority");
+            var sDimLabel = bStatus ? "Status" : "Priority";
+
+            oViz.setVizType(sKey);
+            oViz.removeAllFeeds();
+
+            if (sKey === "pie" || sKey === "donut") {
+                oViz.addFeed(new FeedItem({ uid: "color", type: "Dimension", values: [sDimLabel] }));
+                oViz.addFeed(new FeedItem({ uid: "size",  type: "Measure",   values: ["SLA Hours"] }));
+            } else {
+                oViz.addFeed(new FeedItem({ uid: "valueAxis",    type: "Measure",   values: ["SLA Hours"] }));
+                oViz.addFeed(new FeedItem({ uid: "categoryAxis", type: "Dimension", values: [sDimLabel] }));
+            }
         }
     });
 });
